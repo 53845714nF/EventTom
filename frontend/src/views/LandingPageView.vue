c4512e27eeb9412912c5c55859b7a2bc1fbc3e42
arneallwardt <132394779+arneallwardt@users.noreply.github.com>
2024-11-11 14:14:34 +0100

    feat: Added color variables and Design Guideline doc: Added documentation for Design / Coding Best Practices fix: removed unecassary Views and components

---
+++ frontend/src/views/LandingPageView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class="h1land">Landing Page</h1>
+    </div>
+</template>
+
+<style scoped>
+.h1land {
+    font-size: 50px;
+    color: var(--vt-c-indigo);
+    text-align: center;
+}
+</style>
