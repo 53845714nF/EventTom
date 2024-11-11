6e42ca9f1e78c82b9908bfc4bcd98d2f5415bad4
arneallwardt <132394779+arneallwardt@users.noreply.github.com>
2024-11-11 12:57:34 +0100

    fix: reverted changes on App.vue and main.js

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,43 +1,85 @@
-<!-- src/App.vue -->
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <div id="app">
-    <AppDashboard />
-    <EventForm />
-    <TicketPurchase :events="events" />
-    <CustomerManagement :customers="customers" />
-    <AppNotifications :notifications="notifications" />
-  </div>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
 
-<script>
-import AppDashboard from './components/AppDashboard.vue';
-import EventForm from './components/EventForm.vue';
-import TicketPurchase from './components/TicketPurchase.vue';
-import CustomerManagement from './components/CustomerManagement.vue';
-import AppNotifications from './components/AppNotifications.vue';
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
 
-export default {
-  name: 'App',
-  components: {
-    AppDashboard,
-    EventForm,
-    TicketPurchase,
-    CustomerManagement,
-    AppNotifications
-  },
-  data() {
-    return {
-      events: [
-        { id: 1, name: "Event 1", availableTickets: 20 },
-        { id: 2, name: "Event 2", availableTickets: 15 }
-      ],
-      customers: [
-        { id: 1, name: "Customer 1", lastPurchaseDate: "2023-01-01" }
-      ],
-      notifications: [
-        { id: 1, message: "New Event Added!" }
-      ]
-    };
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
   }
-};
-</script>
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
