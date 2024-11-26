32c9a7c99f1518fe324876bf8fd70a47f44a93e0
arneallwardt <132394779+arneallwardt@users.noreply.github.com>
2024-11-26 12:21:06 +0100

    feat: calculating predicted no tickets sold based on threshold of 0.8

--- frontend/src/constants/DevVariables.js
+++ frontend/src/constants/DevVariables.js
@@ -3,4 +3,5 @@ import { Roles } from "./Roles";
 export default {
   LOGIN_REDIRECT: "/event-manager/events",
   INITIAL_ROLE: Roles.EVENT_MANAGER,
+  TICKET_THRESHOLD: 0.8,
 };
--- frontend/src/services/EventManagerService.js
+++ frontend/src/services/EventManagerService.js
@@ -6,9 +6,15 @@ export default class EventManagerService {
     return  Math.round((sold / no_tickets) * 100);
   }
 
-  static getPercentageOfTicketsSoldComparedToExpected(actual, expected) {
-    return Math.round(((actual - expected) / expected) * 100);
+  // returns how many tickets are sold in relation to the threshold as a percentage
+  static getPercentageOfTicketsSoldComparedToExpected(noTickets, noTicketsSold, threshold) {
+    const expectedNoTicketsSold = noTickets * threshold;
+    return Math.round(((noTicketsSold - expectedNoTicketsSold) / expectedNoTicketsSold) * 100);
   }
+
+  // static getPercentageOfTicketsSoldComparedToExpected(actual, expected) {
+  //   return Math.round(((actual - expected) / expected) * 100);
+  // }
 
   static getHighlightClass(percentage) {
     if (percentage >= 10) {
