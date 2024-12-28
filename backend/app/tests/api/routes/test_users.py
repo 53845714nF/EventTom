9afb6be0df902e263a07575cebec51812f1140b0
LuchsHub <robinwagner1910.rw@gmail.com>
2024-12-28 02:04:11 +0100

    Adapt test_delete_customer_without_privileges to new error message

--- backend/app/tests/api/routes/test_users.py
+++ backend/app/tests/api/routes/test_users.py
@@ -481,4 +481,4 @@ def test_delete_customer_without_privileges(
         headers=normal_user_token_headers,
     )
     assert r.status_code == 403
-    assert r.json()["detail"] == "The user doesn't have enough privileges"
+    assert r.json()["detail"] == "Customers cannot delete users"
