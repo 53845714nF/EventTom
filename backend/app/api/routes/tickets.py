53fdc6358d0e75bc5dd2b1ee5c7c835f49474f8b
LuchsHub <robinwagner1910.rw@gmail.com>
2025-01-04 13:54:26 +0100

    Fix return Sequence of read_manager_ticket_purchases, add purchase_date in buy_ticket response

--- backend/app/api/routes/tickets.py
+++ backend/app/api/routes/tickets.py
@@ -63,11 +63,8 @@ def read_manager_ticket_purchases(
 
     ticket_purchases = [
         TicketPurchaseResponse(
-            ticket_id=ticket.ticket_id,
-            event_id=event.id,
-            event_title=event.title,
-            user_id=user.id,
-            user_email=user.email,
+            user=user,
+            event=event,
             quantity=ticket.quantity,
             purchase_date=ticket.purchase_date,
         )
@@ -143,5 +140,5 @@ async def buy_ticket(
         }
     )
     return TicketPurchaseResponse(
-        user=current_user, event=event, quantity=request.quantity
+        user=current_user, event=event, quantity=request.quantity, purchase_date=ticket.purchase_date
     )
